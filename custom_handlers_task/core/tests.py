990eb39438d2c1da135d679135aa3ecde1e4a1b1
Владимир Козлов <kit.jke@gmail.com>
2022-08-12 00:23:20 +0300

    flake8

--- custom_handlers_task/core/tests.py
+++ custom_handlers_task/core/tests.py
@@ -3,6 +3,15 @@ from django.test import TestCase
 
 class ViewTestClass(TestCase):
     def test_error_page(self):
-        response = self.client.get('/nonexist-page/')
+        url = '/nonexist-page/'
+        template = 'core/404.html'
+        response = self.client.get(url)
+        
+        
         # Проверьте, что статус ответа сервера - 404
+        self.assertEqual(response.status_code, 404)
         # Проверьте, что используется шаблон core/404.html
+        self.assertTemplateUsed(response,template)
+        
+        
+    
